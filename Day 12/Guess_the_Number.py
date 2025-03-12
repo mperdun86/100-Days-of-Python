6c5e5948e6afd35b671a8e7b4fc77a3f143100c9
Matt Perdun <mperdun86@gmail.com>
2025-03-11 21:05:57 -0400

    Guess the Number now functional

--- Day 12/Guess_the_Number.py
+++ Day 12/Guess_the_Number.py
@@ -1,16 +1,12 @@
 import random
-from operator import truediv
-
 from art import logo
-
-print(f"{logo}\nWelcome to guess the number!\n")
 
 def set_number():
     return random.randint(1, 100)
 
 def set_difficulty():
     while True:
-        difficulty_choice = input("Select a difficulty level\nPlease type 'easy', 'normal', or 'hard").lower().strip()
+        difficulty_choice = input("Select a difficulty level\nPlease type 'easy', 'normal', or 'hard': ").lower().strip()
         if difficulty_choice == 'easy':
             return 20
         elif difficulty_choice == 'normal':
@@ -18,22 +14,61 @@ def set_difficulty():
         elif difficulty_choice == 'hard':
             return 5
         else:
-            print("Invalid input!")
+            print("Invalid input.\n")
 
-def attempt_processing(goal):
+def attempt_processing(goal, difficulty):
+    attempts_remaining = difficulty
     while True:
-        #!!!!!!!!!!!!!!!!!!!!!!LEFT OFF HERE!!!!!!!!!!!!!!!!!!!!!!!
-        Guess = input("Please guess a number.")
+        try:
+            #!!!!!!!!!!!!!CHEAT FOR TESTING!!!!!!!!
+            print(f"CHEAT ACTIVE: THE NUMBER IS {goal}")
+            print(f"Attempts remaining: {attempts_remaining}")
+            guess = int(input("Please guess a number between 1 and 100: ").strip())
+            if not(1 <= guess <= 100):
+                print("Invalid input.\n")
+            else:
+                if guess > goal:
+                    print("Too high!\n")
+                    attempts_remaining -= 1
+                    if attempts_remaining <= 0:
+                        print("You have lost!\n")
+                        return
 
+                elif guess < goal:
+                    print("Too low!\n")
+                    attempts_remaining -= 1
+                    if attempts_remaining <= 0:
+                        print("You have lost!\n")
+                        return
+                else:
+                    print("You guessed the answer!\n")
+                    return
 
-def main_game_loop():
-    attempts = set_difficulty()
+        except ValueError:
+            print("Invalid input.\n")
+
+def main_game_start():
+    difficulty = set_difficulty()
     target_number = set_number()
-    game_active = True
-    while game_active:
-        attempt_processing(target_number)
+    attempt_processing(target_number, difficulty)
+
+def new_game_choice():
+    while True:
+        choice = input("Would you like to play again?\nPlease type 'yes' or 'no': ").lower().strip()
+        if choice == "yes":
+            return
+        if choice == "no":
+            input("Thank you for playing Guess the Number, please hit enter to shut down the program")
+            exit()
+        else:
+            print("Invalid input.")
 
 
+# Main Loop
+while True:
+    print(f"{logo}\nWelcome to Guess the Number!\n")
+    main_game_start()
+    new_game_choice()
 
 
 
