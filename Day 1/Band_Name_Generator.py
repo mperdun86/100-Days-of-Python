37cb65e25516841244d51d9979875c82e563d9e7
Matt Perdun <mperdun86@gmail.com>
2025-03-13 20:06:47 -0400

    test

--- Day 1/Band_Name_Generator.py
+++ Day 1/Band_Name_Generator.py
@@ -5,4 +5,4 @@ city_name = input("What city did you grow up in?\n")
 #Ask the user for the name of a pet and store it in a variable.
 pet_name = input("What is the name of a childhood pet?\n")
 #Combine the name of their city and pet and show them their band name.
-print(f"The name of your band could be \"{city_name} {pet_name}\" ")
+print(f"The name of your band should be \"{city_name} {pet_name}\" ")
